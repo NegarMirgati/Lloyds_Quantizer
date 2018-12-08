f779a867c12a2219201f4df30eeb07b16deef5c2
NegarMirgati <negar.nbj95@gmail.com>
2018-12-08 23:41:14 +0330

    Fix ifelse :|

--- getSamplesBetween.m
+++ getSamplesBetween.m
@@ -6,22 +6,24 @@ function samplesBetween = getSamplesBetween(samples, lowerBound, upperBound)
         if(samples(1, i) > lowerBound && samples(1, i) < upperBound)
             temp = [temp, samples(1, i)];
         end
+    end
         
     if(isempty(temp))
         if(lowerBound ~= Inf && upperBound ~= Inf)
             newRand = (upperBound - lowerBound)* normrnd(0,1) + lowerBound;
             temp = [temp, newRand];
-        else if (lowerBound == -Inf) 
+  
+        elseif (lowerBound == -Inf) 
              newRand = (upperBound - upperBound/2)* normrnd(0,1) + upperBound/2;
              temp = [temp, newRand];
-        else if (upperBound == Inf) 
+
+            
+        elseif (upperBound == Inf) 
              newRand = (2 * lowerBound - lowerBound)* normrnd(0,1) + lowerBound;
              temp = [temp, newRand];
-            end
-            end
+            
         end
     end
     samplesBetween = temp;
-end
 end
 
